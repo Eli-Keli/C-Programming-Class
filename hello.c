16a0af7c4b56f77eb3e229a3ebd70b8741ad761a
Eli Keli <muthokaelikeli@gmail.com>
2024-06-06 17:00:43 +0300

    Added hello.c file

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // Include the standard input-output header file
+
+int main() { // Start of the main function
+    printf("This is my firt C Code\n");// Print "This is my firt C Code" followed by a newline
+    printf("My name is Eli Keli\n");
+    printf("His name is Philip\n");
+    printf("His name is Darwins");
+    return 0;// indicate successful completion of the program
+}
